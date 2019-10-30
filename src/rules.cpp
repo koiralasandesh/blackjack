b3618fc63d6a0e81d646b3cff455a408d524ce83
Himal Basnet <himalbasnet9@gmail.com>
2019-10-30 14:13:46 -0500

    added some rules text

--- src/rules.cpp
+++ src/rules.cpp
@@ -8,7 +8,13 @@ Rules_window::Rules_window() : box(Gtk::ORIENTATION_VERTICAL)
 
     add(box);
 
-    rules_list.set_text("List of rules to play black jack.");
+    rules_list.set_text("List of rules to play black jack.
+    1. Hand sign must be used by player as guided by the dealer. Player are not allowed to touch cards.
+    2. The minimum bet player can do in $1 and max bet is $5.
+    3. Will be payed 1.5 times the bet if player got blackjack.
+    4. Players are strictly restricted to count cards.
+    5. Dealer has to hit always till it reach soft 17.s
+    ");
     rules_list.set_padding(5, 5);
     box.pack_start(rules_list);
 
